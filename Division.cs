f4c28e15efea0e9a45816d2fa0c84686ada52fd3
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:10:11 +0530

    Initial Push from local

---
+++ Division.cs
@@ -0,0 +1,33 @@
+﻿namespace BasicPractiseProblems
+{
+    class Division
+    {
+        public static void GetQuotientRemainder()
+        {
+            Console.WriteLine("Question 6 : Compute Quotient and Remainder \n");
+
+            try
+            {
+                Console.Write("Input 1st number : ");
+                var num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input 2nd number : ");
+                var num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (num1 < 1 || num2 < 1)
+                    throw new Exception();
+                
+                var quotient = num1 / num2;
+                var remainder = num1 % num2;
+
+                Console.WriteLine("\n >>>>> Division of {0} by {1} yields : \n\n >>>>> Quotient : {2} \n >>>>> Reaminder : {3}", num1, num2, quotient,remainder);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n!!! INVALID INPUT !!!\n");
+                GetQuotientRemainder();
+            }
+
+        }
+    }
+}
