90fd04125bd4d97a8b361ca08f01e8ae02cefaa6
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:10:48 +0530

    Initial Push from local

---
+++ PowerOf2s.cs
@@ -0,0 +1,29 @@
+﻿namespace BasicPractiseProblems
+{
+    class PowerOf2s
+    {
+        public static void Get()
+        {
+            Console.WriteLine("Question 3 : Power of 2s (restrict at overflow of int) \n");
+            Console.Write("Enter a number : ");
+
+            try
+            {
+                var num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > 32)
+                    throw new Exception();
+
+                for (var i = 0; i <= num; i++)
+                {
+                    Console.WriteLine(">>>> 2 to the power {0} : {1}", i, Math.Pow(2, i));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!!! INVALID INPUT !!!!");
+                Get();
+            }
+        }
+    }
+}
