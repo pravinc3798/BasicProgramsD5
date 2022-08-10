795785d7ad460746829321599edbdcfa732752f6
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:10:53 +0530

    Initial Push from local

---
+++ PrimeFactors.cs
@@ -0,0 +1,31 @@
+﻿namespace BasicPractiseProblems
+{
+    public class PrimeFactors
+    {
+        public static void GetFactors()
+        {
+            Console.WriteLine("Question 5 : Prime Factors \n");
+            Console.Write("Input a number : ");
+
+            try
+            {
+                var num = Convert.ToInt32(Console.ReadLine());
+
+                if (num < 1)
+                    throw new Exception();
+
+                for (int i = 2; i * i <= num; i++)
+                {
+                    if (num % i == 0)
+                        Console.WriteLine(" >> {0}", i);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n!!! INVALID INPUT !!!\n");
+                GetFactors();
+            }
+
+        }
+    }
+}
