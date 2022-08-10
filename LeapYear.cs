7b460b56a80947f9a0284f21a2156b06d211b495
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:10:42 +0530

    Initial Push from local

---
+++ LeapYear.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicPractiseProblems
+{
+    class LeapYear
+    {
+        public static void Check()
+        {
+            Console.WriteLine("Question 2 : Leap Year check \n");
+            Console.Write("Enter a year : ");
+
+            try
+            {
+                var year = Convert.ToInt32(Console.ReadLine());
+                if (year.ToString().Length == 4)
+                {
+                    var leapChk = (year % 4 == 0) ? string.Format("{0} is a Leap Year", year) : string.Format("{0} is NOT a Leap Year", year);
+                    Console.WriteLine("\n>>>> {0}", leapChk);
+                }
+                else
+                {
+                    Console.WriteLine("\n!!!! ENTER A FOUR DIGIT NUMBER !!!!");
+                    Check();
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!!! INVALID INPUT !!!!");
+                Check();
+            }
+        }
+    }
+}
